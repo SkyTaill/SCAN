d32e9d86048d1f677e26343fe42207a2716b1a97
maxim <vorobeichik2015@yandex.ru>
2023-01-23 23:37:53 +0300

    add header

--- my-app/src/components/Header/index.tsx
+++ my-app/src/components/Header/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
-//import css from "./index.module.css"
+import css from "./index.module.css"
 
 function Header() {
     return (
-        <header className="App">
-            asdasd
+        <header className={css.header}>
+            <div className={css.header_content}>
+                <div className={css.scan}></div>
+                <div className={css.menu}>
+                    <button className={css.menu_item}>Главная</button>
+                    <button className={css.menu_item}>Тарифы</button>
+                    <button className={css.menu_item}>FAQ</button>
+                </div>
+                <div className={css.loginOrRegistrate}>
+                    <div className={css.registate}>Зарегистрироваться</div>
+                    <div className={css.line}></div>
+                    <button className={css.button_login}>Войти</button>
+                </div>
+            </div>
         </header>
     );
 }
