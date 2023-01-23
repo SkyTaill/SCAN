40f04a4988f3a6c1566a257667820d0e113db642
maxim <vorobeichik2015@yandex.ru>
2023-01-24 02:02:39 +0300

    add main- carousel block

--- my-app/src/components/HomePage/index.tsx
+++ my-app/src/components/HomePage/index.tsx
@@ -4,15 +4,23 @@ import css from "./index.module.css"
 
 function Home() {
     return (
-        <div className={css.flex_block}>
-            <div className={css.flex_text_block}>
-                <h1 className={css.capital_h1_text}>сервис по поиску публикаций о компании по его ИНН</h1>
-                <p className={css.text_p_search_info}>Комплексный анализ публикаций, получение данных в формате PDF на электронную почту.</p>
-                <button className={css.buttom_get_date}>Запросить данные</button>
-            </div>
-            <div className={css.backround_img}>
+        <div>
+            <section className={css.flex_block}>
+                <div className={css.flex_text_block}>
+                    <h1 className={css.capital_h1_text}>сервис по поиску публикаций о компании по его ИНН</h1>
+                    <p className={css.text_p_search_info}>Комплексный анализ публикаций, получение данных в формате PDF на электронную почту.</p>
+                    <button className={css.buttom_get_date}>Запросить данные</button>
+                </div>
+                <div className={css.backround_img}>
+                </div>
+            </section>
 
-            </div>
+            <section className={css.flex_block_corusel}>
+                <h2 className={css.capital_h2_text} >Почему именно мы</h2>
+                <div>333</div>
+            </section>
+
+
         </div>
     );
 }
