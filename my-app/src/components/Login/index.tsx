b8972456a51313a0b1e134e8d427e9d74c35e6f6
maxim <vorobeichik2015@yandex.ru>
2023-01-31 03:08:22 +0300

    add valid in login

--- my-app/src/components/Login/index.tsx
+++ my-app/src/components/Login/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { useState } from 'react';
+//import { json } from 'stream/consumers';
 import css from "./index.module.css"
-
-
 
 
 function Login() {
     const [loginBoolen, setLoginBoolen] = useState(false)
     const [passwordBoolen, setPasswordBoolen] = useState(false)
-    const [login, setLogin] = useState()
-    const [password, setPassword] = useState()
+    const [login, setLogin] = useState("")
+    const [password, setPassword] = useState("")
+    // const [error, setError] = useState(false)
 
     const onChangeLogin = (e: React.FormEvent<HTMLInputElement>) => {
         var a: boolean
@@ -25,10 +25,14 @@ function Login() {
             //  console.log(patt.test(e.currentTarget.value))
             a = patt.test(e.currentTarget.value)
             setLoginBoolen(a)
+            if (a) {
+                setLogin(e.currentTarget.value)
+            }
         } else if (e.currentTarget.value === "") {
             setLoginBoolen(false)
         } else {
             setLoginBoolen(true)
+            setLogin(e.currentTarget.value)
         }
 
     }
@@ -39,7 +43,68 @@ function Login() {
             setPasswordBoolen(false)
         } else {
             setPasswordBoolen(true)
+            setPassword(e.currentTarget.value)
         }
+    }
+
+    const onClick = async () => {
+        if (loginBoolen !== false && passwordBoolen !== false) {
+
+
+            try {
+                const options = {
+                    // Будем использовать метод POST
+                    method: 'POST',
+                    // Добавим тело запроса
+                    body: JSON.stringify({
+                        login: login,
+                        password: password
+
+                    }),
+                    // Дополнительный заголовое с описанием типа данных,
+                    // которые мы отправляем серверу. В данном случае
+                    // сервер jsonplaceholder будет знать, как ему
+                    // обрабатывать запрос
+                    headers: {
+                        "Content-type": "application/json; charset=UTF-8"
+                    }
+                }
+                // Делаем запрос за данными
+                await fetch('https://gateway.scan-interfax.ru/api/v1/account/login', options)
+                    .then(response => {
+                        if (!response.ok) {
+                            //  setError( response.json.)
+                            // response.json()
+                            throw new Error('Error occurred!')
+
+                        }
+                        return response.json()
+                    })
+                    .then(json => {
+                        var a2: string = json.accessToken;
+                        console.log("Token--", a2)
+                    }
+
+                    ).catch((err) => {
+                        //   setError(false)
+                        setPasswordBoolen(false)
+                        setLoginBoolen(false)
+                        setPassword("")
+                        setLogin("")
+                        console.log(err, "error")
+                    })
+
+            } catch {
+                setPasswordBoolen(false)
+                setLoginBoolen(false)
+                setPassword("")
+                setLogin("")
+                console.log("errrer")
+            }
+
+
+        }
+
     }
     return (
         <div>
@@ -82,11 +147,11 @@ function Login() {
                         <p className={css.textLoginBox} style={{ marginTop: "40px" }}>Пароль:</p>
                         <input type="password" className={passwordBoolen ? css.input : css.inputErr} onChange={onChangePassword} />
 
-                        {passwordBoolen ? null : (<p className={css.textErr}>Неправильный</p>)}
+                        {passwordBoolen ? null : (<p className={css.textErr}>Неправильный пароль</p>)}
 
 
-                        {loginBoolen ? (passwordBoolen ? <button className={css.buttonLogInValidTrue} style={{ marginTop: "40px" }}>Войти</button>
-                            : <button className={css.buttonLogIn} style={{ marginTop: "40px" }}>Войти</button>) : <button className={css.buttonLogIn} style={{ marginTop: "40px" }}>Войти</button>
+                        {loginBoolen ? (passwordBoolen ? <button className={css.buttonLogInValidTrue} onClick={onClick} style={{ marginTop: "40px" }}>Войти</button>
+                            : <button className={css.buttonLogIn} style={{ marginTop: "40px" }}>Войти</button>) : <button className={css.buttonLogIn} style={{ marginTop: "40px" }} >Войти</button>
                         }
 
 
