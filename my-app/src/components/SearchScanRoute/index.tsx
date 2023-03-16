b517851e9144ff87f2c359e5caf872a5730920fb
maxim <vorobeichik2015@yandex.ru>
2023-03-16 03:02:36 +0300

    add element block (mini Article)

--- my-app/src/components/SearchScanRoute/index.tsx
+++ my-app/src/components/SearchScanRoute/index.tsx
@@ -48,6 +48,30 @@ const SearchScanRoute: React.FC<SearchScanRouteProps> = ({ autorize }) => {
                         <div className={css.rightArrow}></div>
                     </div>
                 </div>
+                <div className={css.centr}>
+                    <h1 className={css.capital_h1_text}>
+                        Список документов
+                    </h1>
+                    <div className={`${css.documentMargin} ${css.centr}`}>
+                        <GetListOfDockument
+                            img={'https://storage.scan-interfax.ru/images/1%3A0JPQqdGM0JNWCdCzf2Jt0LHQotGV0ZUh0ZbRlBXCt0Je0JHQruKAnDcUXkZQ0YvQscKn0KjQlsKu0K%2FSkdGXfOKAsF3QkjrRnCRmGCFFBybQoNGL0ZMhEFkC4oCYaNC9a9GO0KFYwqwOeNGO0JdUDGzihKJXTNC%2B0ZzRinE%3D'}
+                            head={"Скиллфэктори - лучшая онлайн-школа для будущих айтишников"}
+                            text={"Кто такой Data Scientist и чем он занимается? Data Scientist — это специалист, который работает с большими массивами данных, чтобы с их помощью решить задачи бизнеса. Простой пример использования больших данных и искусственного интеллекта — умные ленты в социальных сетях. На основе ваших просмотров и лайков алгоритм выдает рекомендации с контентом, который может быть вам интересен. Эту модель создал и обучил дата-сайентист, и скорее всего, не один.В небольших компаниях и стартапах дата-сайентист делает все: собирает и очищает данные, создает математическую модель для их анализа, тестирует ее и презентует готовое решение бизнесу"}
+                            tag={"Технические новости"}
+                            words={123}
+                        />
+                        <GetListOfDockument
+                            img={'https://storage.scan-interfax.ru/images/1%3A0JPQqdGM0JNWCdCzf2Jt0LHQotGV0ZUh0ZbRlBXCt0Je0JHQruKAnDcUXkZQ0YvQscKn0KjQlsKu0K%2FSkdGXfOKAsF3QkjrRnCRmGCFFBybQoNGL0ZMhEFkC4oCYaNC9a9GO0KFYwqwOeNGO0JdUDGzihKJXTNC%2B0ZzRinE%3D'}
+                            head={"fdsaadasdasd"}
+                            text={"Кто такой Data Scientist и чем он занимается? Data Scientist — это специалист, который работает с большими массивами данных, чтобы с их помощью решить задачи бизнеса. Простой пример использования больших данных и искусственного интеллекта — умные ленты в социальных сетях. На основе ваших просмотров и лайков алгоритм выдает рекомендации с контентом, который может быть вам интересен. Эту модель создал и обучил дата-сайентист, и скорее всего, не один.В небольших компаниях и стартапах дата-сайентист делает все: собирает и очищает данные, создает математическую модель для их анализа, тестирует ее и презентует готовое решение бизнесу"}
+                            tag={"Технические новости"}
+                            words={123}
+                        />
+
+                    </div>
+
+                </div>
+                <button className={css.section_button}>Показать больше</button>
             </section>
         )
     }
@@ -104,6 +128,44 @@ const ShortStatisticElement: React.FC<ShortStatisticElementProps> = ({ dateStart
                 <div className={css.line}></div>
 
             </div>
+
+        </div>
+
+
+    )
+}
+
+interface GetListOfDockumentProps {
+    img: string,
+    head: string,
+    text: string,
+    tag: string,
+    words: number
+
+}
+const GetListOfDockument: React.FC<GetListOfDockumentProps> = ({ img, head, text, tag, words }) => {
+    return (
+
+        <div className={css.element}>
+            <div className={css.element_box}>
+                <h1 className={css.h1_element}>{head}</h1>
+                <div className={css.element_yellowBlock}>
+                    <p className={`${css.element_yellowBlock_p_text} `}>{tag}</p>
+                </div>
+                <img className={css.element_img} src={img} />
+
+                <p className={css.element_p_boodytext}>{text}</p>
+
+                <div className={css.flex}>
+                    <button className={css.element_button}>Читать в источнике</button>
+                    <h6 className={css.element_h6_text}>{words} слов</h6>
+                </div>
+
+
+
+            </div>
+
+
 
         </div>
 
